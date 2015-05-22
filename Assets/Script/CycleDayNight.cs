352e09805617e7995b7a8cdee633c8b16dd5e8e0
Baptiste <b.billet@rubika-edu.com>
2015-05-22 14:01:59 +0200

    save

---
+++ Assets/Script/CycleDayNight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleDayNight : MonoBehaviour {
+
+
+	public float m_RotatePerSecond;
+	private float m_EulerX;
+	// Use this for initialization
+	void Start () {
+		this.transform.eulerAngles = new Vector3(90, 0, 0);
+		m_EulerX = 90;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_EulerX += m_RotatePerSecond;
+		this.transform.eulerAngles = new Vector3(m_EulerX, 0, 0);
+	}
+}
