564e6e41b275f272b126819f5951422664bc490a
Baptiste <b.billet@rubika-edu.com>
2015-05-27 22:51:03 +0200

    Bug lol

--- Assets/Script/LoadLevel.cs
+++ Assets/Script/LoadLevel.cs
@@ -15,7 +15,6 @@ public class LoadLevel : MonoBehaviour {
 	public string m_SaveName;
 	private string m_SaveData;
 
-
 	private string m_ShortMemory;
 
 	// Use this for initialization
@@ -24,20 +23,49 @@ public class LoadLevel : MonoBehaviour {
 		m_SaveData=System.IO.File.ReadAllText(Application.dataPath + "\\SaveLevel\\" + m_SaveName + ".txt");
 		System.IO.StringReader sr = new StringReader(m_SaveData);
 
-		
+		//On vide la mémoire
+		m_ShortMemory = null;
 
-		while(sr.Read().Parse!=';')
+
+
+		//Taille du fichier
+		//On prend le premier charactere
+		while ((char)sr.Peek() != ';') //On test on ne parcours pas.
 		{
-
+			m_ShortMemory += ((char)sr.Read()).ToString(); //On rajoute ce que l'on lis dans la mémoire
 		}
+		m_LevelSize = int.Parse(m_ShortMemory);
 
+		Debug.Log(m_ShortMemory);
+		m_ShortMemory += ((char)sr.Read()).ToString(); //On rajoute ce que l'on lis dans la mémoire
+		Debug.Log(m_ShortMemory.Length - 1);
+		Debug.Log(m_ShortMemory.Remove(m_ShortMemory.Length - 1));
+		m_ShortMemory.Remove(m_ShortMemory.Length-1);
+		Debug.Log(m_ShortMemory);  //NON MAIS LOL
+
+
+		//Placement dans le fichier
+		//Tant que nous n'avons pas fini le fichier
+		while((char)sr.Peek()!='}') //On test on ne parcours pas.
+		{
+			m_ShortMemory+=((char)sr.Read()).ToString(); //On rajoute ce que l'on lis dans la mémoire
+			if(m_ShortMemory.Contains(";"))//Si on arrive à une fin d'information
+			{
+				//On enleve alors le ;
+				m_ShortMemory.Remove(m_ShortMemory.Length - 1);
+			}
+			
+		}
+		/*
 		for (int i = 0; i < m_LevelSize; i++)
 		{
 			for (int y = 0; y < m_LevelSize; y++)
 			{
 
 			}
-		}
+		}*/
+		
+		 
 
 	}
 	
